ecdb9300c74ea5a90234ce90b472adda4b08ce39
Pratima-Prabhu <53810643+Pratima-Prabhu@users.noreply.github.com>
2019-09-09 21:07:32 +0530

    updated app.js

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express=require("express");
 var app=express();
 
+
+
+app.use(express.static("public")); 
+
 var request=require("request");
 app.set("view engine","ejs");
 
@@ -22,7 +26,20 @@ app.get("/results",function(req,res){
 });
 
 
+app.get("/results/:id",function(req,res){
+	var query=req.query.id;
+	var url="http://www.omdbapi.com/?i="+query+"&apikey=thewdb&Plot=full";
+	request(url,function(error,response,body){
+		if(!error && response.statusCode==200){
+			var data=JSON.parse(body);
+			res.render("show",{data:data});
+		}
+	});
+	
+});
 
-app.listen(3000,()=>{
+
+
+app.listen(8081,()=>{
 	console.log("Server Strated");
 });
